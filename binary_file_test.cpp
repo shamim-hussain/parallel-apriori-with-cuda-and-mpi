83f1fac622c492da98e419128ad99c7be042204a
Shamim Hussain <snirjhar@gmail.com>
2020-04-29 14:50:39 -0400

    got file len correctly

--- binary_file_test.cpp
+++ binary_file_test.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char* argv[]){
+    ifstream file ("mnist_train_25.dat",ios::binary|ios::ate);
+    int file_len;
+    char* buffer;
+
+    if (file.is_open()){
+        file_len = file.tellg();
+        file.seekg(0,ios::beg);
+        buffer = new char[file_len];
+        file.read(buffer,file_len);
+        file.close();
+    } else {
+        cout<<"Failed to open file!!"<<endl;
+    }
+
     
+    cout<<"Length of the file = "<<file_len<<endl;
     return 0;
 }
