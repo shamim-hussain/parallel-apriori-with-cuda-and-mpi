c25715561f4e1d195b24c512d6aba1a1c8ebd090
Hussain <DLTMhssn@erpfen01.ccni.rpi.edu>
2020-05-03 01:57:30 -0400

    Mpi file read working

--- test_apriori.cpp
+++ test_apriori.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-#define _MPI_DTYPE MPI_UNSIGNED 
+#define _MPI_ELM_DTYPE MPI_CHAR
+#define _MPI_SUP_DTYPE MPI_UNSIGNED 
 
 #define _FILE_NAME "mnist_train_25.dat"
 #define _TRANS_LEN 25
@@ -32,6 +33,11 @@ int main(int argc, char* argv[]){
     size_t minsup = _MINSUP;
     const char* file_name = _FILE_NAME;
     unsigned int num_threads = _NUM_THREADS;
+    
+    MPI_File in_file;
+    MPI_Offset filesize;
+    MPI_Status mpistat;
+
     // Size of the MPI COMM
     int g_mpiSize=1;
     // Rank of the process in MPI
@@ -44,30 +50,35 @@ int main(int argc, char* argv[]){
 
     cuda_init(g_mpiRank);
     
-    MPI_File cFile;
 
-    if (MPI_File_open(MPI_COMM_WORLD, file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &cFile )) {
+    if (MPI_File_open(MPI_COMM_WORLD, file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &in_file )) {
         cout<< "Unable to open file!"<<endl;
         exit(-1);
     }
-    
-    MPI_Offset filesize;
-    MPI_File_get_size(cFile, &filesize);
 
-    MPI_File_close( &cFile );
+    
+    MPI_File_get_size(in_file, &filesize);
+
+    size_t tot_trans = filesize/trans_len;
+
+    dtype dat(filesize);
+
+    MPI_File_read(in_file, dat.data(), filesize, _MPI_ELM_DTYPE, &mpistat);
+
+    MPI_File_close(&in_file);
     
     cout<<"File Size = "<<filesize<<endl;
 
-    ifstream file (file_name,ios::binary);
+    // ifstream file (file_name,ios::binary);
 
-    if (!file.is_open()){
-        cout<<"Failed to open file!!"<<endl;
-        exit(-1);
-    }
+    // if (!file.is_open()){
+    //     cout<<"Failed to open file!!"<<endl;
+    //     exit(-1);
+    // }
 
-    istreambuf_iterator<etype> begin(file), end;
-    dtype dat(begin,end);
-    file.close();
+    // istreambuf_iterator<etype> begin(file), end;
+    // dtype dat(begin,end);
+    // file.close();
 
     Dataset dataset(trans_len);
     dataset.swap_data(dat);
@@ -90,7 +101,7 @@ int main(int argc, char* argv[]){
         compute.get_supports(apriori.supports.data());
 
         MPI_Allreduce(MPI_IN_PLACE,apriori.supports.data(), 
-                        apriori.supports.size(), _MPI_DTYPE, 
+                        apriori.supports.size(), _MPI_SUP_DTYPE, 
                         MPI_SUM, MPI_COMM_WORLD);
                         
         apriori.remove_infrequent();
